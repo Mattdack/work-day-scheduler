f5b285b6a39efe913b84ba3dc928370141cdb620
Matthew Dacanay <mattdack@uw.edu>
2022-10-06 16:44:02 -0700

    Provides the functionality for the save buttons, local storage, and rendering past user inputs.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # work-day-scheduler
+
+## Description 
+
+This [Work Day Scheduler](mattdack.github.io/work-day-scheduler) provides an interface for users to enter and save events or tasks during work-day hours, save them, and reload the page to view them again. On page load the site updates the date. The background for each input section is updated with a background color to indicate whether the time slot is in the past, present, and future. The site utilizes moment.js, event listening, and local storage to achieve this functionality. The site is styled by bootstrap with additional custom styling.
+
+## Screenshot of Webpage
+<img src="./assets/images/Website-Mock-Up.png" alt="Screenshot of Webpage">
+
+
+## Installation
+
+There is no installation required or capable with this repository. Running the index.html file in a default browser renders a landing page that presents the scheduler.
+
+
+## Usage 
+
+Users can select the grey, red, or green areas on the page and type any needed input. Clicking a save button at the end of the page will save the text in every single input area on the page. Reloading the page will render an updated date, backgrounds for each hour, 
+
+## Credits
+
+ Trilogy in collaboration with the University of Washington provided the initial HTML and CSS files. Matthew Dacanay completed these files and then wrote the entirety of the javascript functionality of this webpage. Technical concepts and public resources were referred to as needed. You can find other projects by Matthew at his [github page](github.com/mattdack).
--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,61 @@
 console.log("Linked");
-var clockEl = $('#currentDay');
+var clockEl = $("#currentDay");
+var plannerPageEl = $("#planner-page");
 var hours = [
-    "#eight",
-    "#nine",
-    "#ten",
-    "#eleven",
-    "#twelve",
-    "#thirteen",
-    "#fourteen",
-    "#fifteen",
-    "#sixteen",
-    "#seventeen"
-]
+  "#eight",
+  "#nine",
+  "#ten",
+  "#eleven",
+  "#twelve",
+  "#thirteen",
+  "#fourteen",
+  "#fifteen",
+  "#sixteen",
+  "#seventeen",
+];
 var currentTime;
 var currentHour = parseInt(moment().format("H"));
-
+var events = ["", "", "", "", "", "", "", "", "", ""];
 
 function init() {
-currentTime = moment().format("MMM Do, YYYY");
-clockEl.text(currentTime);
-console.log(currentHour);
+  currentTime = moment().format("MMM Do, YYYY");
+  clockEl.text(currentTime);
+  console.log(currentHour);
 
-//Update the background on the event sections of each time bar.
-for (var i = 0; i < hours.length; i++) {
-    var hourCompare = parseInt(moment(8+i, "H").format("H"));
+  //Update the background on the event sections of each time bar.
+  for (var i = 0; i < hours.length; i++) {
+    var hourCompare = parseInt(moment(8 + i, "H").format("H"));
     if (hourCompare > currentHour) {
-        $(hours[i]).removeClass("past");
-        $(hours[i]).addClass("future");
-    } else if (hourCompare === currentHour){
-        $(hours[i]).removeClass("past");
-        $(hours[i]).addClass("present");
+      $(hours[i]).removeClass("past");
+      $(hours[i]).addClass("future");
+    } else if (hourCompare === currentHour) {
+      $(hours[i]).removeClass("past");
+      $(hours[i]).addClass("present");
     }
+  }
+  getStoredEvents();
 }
 
-// On page load check if there are any stored values for the planner hours.
-// If not null set the placeholder text for each input to the stored values.
+function getStoredEvents() {
+  if (localStorage.getItem("events") !== null) {
+    events = JSON.parse(localStorage.getItem("events"));
+  }
+  for (let i = 0; i < events.length; i++) {
+    $(hours[i]).val(events[i])
+  }
+}
 
-//Event listener for the separate save buttons. When button is clicked, store the input value in local storage. Set the placeholder value to the locally stored value.
-
-// If each button has an ID, then you can use event.target to be able to save the section with the same time.
-
+function setStoreEvents(event) {
+  event.preventDefault();
+    console.log(event.target.tagName);
+  if (event.target.tagName === "BUTTON" || event.target.tagName === "I") {
+    for (let i = 0; i < events.length; i++) {
+        console.log("Storing each event now.")
+      events[i] = $(hours[i]).val();
+      localStorage.setItem("events", JSON.stringify(events));
+    }
+  }
 }
 
 init();
+plannerPageEl.on("click", setStoreEvents);
